586f3ee91769b628c88dfdb4de5ee6a501e59d6e
Terence Sun <tsun1215@gmail.com>
2015-04-25 19:40:38 -0400

    Added gradle support

---
+++ src/main/java/edu/cmu/cs/cs214/rec15/Main.java
@@ -0,0 +1,20 @@
+package edu.cmu.cs.cs214.rec15;
+
+/**
+ * Main class for Recitation 15.
+ * 
+ * @author tsun
+ *
+ */
+public class Main {
+    
+    /**
+     * Main function
+     * 
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
