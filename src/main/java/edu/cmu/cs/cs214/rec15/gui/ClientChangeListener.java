dd7886ef0b3030707b14c254507a68105836af45
Nora <nora@mindcrafted.com>
2015-04-25 21:51:29 -0400

    Adding gui package and ClientChangeListener

---
+++ src/main/java/edu/cmu/cs/cs214/rec15/gui/ClientChangeListener.java
@@ -0,0 +1,18 @@
+package edu.cmu.cs.cs214.rec15.gui;
+
+public interface ClientChangeListener {
+	
+	/**
+	 * Called when a user wants to enter the chat room
+	 * @param username of the user
+	 * @param port that the server is listening on 
+	 * @param IP address of the user's computer
+	 */
+	void startChat(String username, String port, String IP);
+	
+	/**
+	 * Called when a new message is received
+	 * @param message text of new message being received
+	 */
+	void messageReceived(String message);
+}
