554d8af6a5db846dc1fafddfb866649f2b1f66ae
Nora <nora@mindcrafted.com>
2015-04-25 23:01:30 -0400

    Adding various JComponents

--- src/main/java/edu/cmu/cs/cs214/rec15/gui/ClientPanel.java
+++ src/main/java/edu/cmu/cs/cs214/rec15/gui/ClientPanel.java
@@ -3,6 +3,11 @@
  */
 package edu.cmu.cs.cs214.rec15.gui;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import edu.cmu.cs.cs214.rec15.client.ChatClient;
 import edu.cmu.cs.cs214.rec15.client.ClientChangeListener;
 
@@ -10,9 +15,21 @@ import edu.cmu.cs.cs214.rec15.client.ClientChangeListener;
  * @author nora
  *
  */
-public class ClientPanel implements ClientChangeListener {
+public class ClientPanel extends JPanel implements ClientChangeListener {
 
 	private ChatClient client; 
+	
+	private final JLabel usernameLabel;
+	private final JLabel portLabel;
+	private final JLabel ipLabel;
+	
+	private final JTextField usernameField;
+	private final JTextField portField;
+	private final JTextField ipField;
+	private final JTextField messageField;
+	
+	private final JButton startButton;
+	private final JButton sendButton;
 	
 	/**
 	 * Constructor for ClientPanel takes in an instance of the ChatClient
@@ -21,6 +38,18 @@ public class ClientPanel implements ClientChangeListener {
 	public ClientPanel(ChatClient chatClient) {
 		this.client = chatClient;
 		chatClient.addClientChangeListener(this);
+		
+		usernameLabel = new JLabel("username: ");
+		portLabel = new JLabel("port: ");
+		ipLabel = new JLabel("server IP: ");
+		
+		usernameField = new JTextField();
+		portField = new JTextField();
+		ipField = new JTextField();
+		messageField = new JTextField();
+		
+		startButton = new JButton("Start");
+		sendButton = new JButton("Send");
 	}
 
 	/* (non-Javadoc)
