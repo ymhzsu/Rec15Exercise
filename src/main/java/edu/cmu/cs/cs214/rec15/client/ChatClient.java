b07dd618ea426f8754230ed2f1da875c84fb0358
Terence Sun <tsun1215@gmail.com>
2015-04-25 22:06:16 -0400

    Added comments to the ChatClient interface

--- src/main/java/edu/cmu/cs/cs214/rec15/client/ChatClient.java
+++ src/main/java/edu/cmu/cs/cs214/rec15/client/ChatClient.java
@@ -1,8 +1,22 @@
 package edu.cmu.cs.cs214.rec15.client;
 
 public interface ChatClient {
-    
+
+    /**
+     * Sends the message the server it is connected to
+     * 
+     * @param message
+     *            Message to be sent
+     * @return True if the message was successfully sent, false otherwise
+     */
     public boolean sendMessage(String message);
-    
+
+
+    /**
+     * Sets the username of the client
+     * 
+     * @param username
+     *            Username of the client
+     */
     public void setUsername(String username);
 }
