c6536cb34cc68e0baeb7ae64202c0573a03d0821
Nora <nora@mindcrafted.com>
2015-04-26 16:05:19 -0400

    Getting rid of unused imports in ChatClientImpl so the build succeeds

--- src/main/java/edu/cmu/cs/cs214/rec15/client/ChatClientImpl.java
+++ src/main/java/edu/cmu/cs/cs214/rec15/client/ChatClientImpl.java
@@ -1,12 +1,9 @@
 package edu.cmu.cs.cs214.rec15.client;
 
-import java.io.BufferedReader;
 import java.io.EOFException;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
