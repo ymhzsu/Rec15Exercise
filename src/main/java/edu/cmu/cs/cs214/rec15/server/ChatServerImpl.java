c844f2d27d1218eec0012ad317fe5349cd3bdc6a
Anny Ni <reminisce.memories@gmail.com>
2015-04-29 12:03:56 -0400

    For each new message, added a server timestamp to it.

--- src/main/java/edu/cmu/cs/cs214/rec15/server/ChatServerImpl.java
+++ src/main/java/edu/cmu/cs/cs214/rec15/server/ChatServerImpl.java
@@ -7,6 +7,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -180,6 +181,7 @@ public class ChatServerImpl extends Thread implements ChatServer {
             // TODO: Add the server timestamp to the message received. Note:
             // Message#setServerTimestamp was created for you in the Message
             // class.
+        	msg.setServerTimestamp(new Date());
 
             // Synchronize because we are iterating through all clients in a
             // thread
